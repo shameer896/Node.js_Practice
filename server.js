25a580907f2fde0bf0ec498c29db7ecaa9dad45d
Shameer Abdul Kader <shameerak896@gmail.com>
2024-01-16 16:56:35 +0530

    respond with html page
    server responding with the html page

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
-const http = require('http')
+const http = require("http");
+const fs = require("fs");
 
-const server = http.createServer((req,res)=>{
-    console.log(req.url,req.method)
+const server = http.createServer((req, res) => {
+  console.log(req.url, req.method);
 
-    res.setHeader('Content-Type','text/html')
+  res.setHeader("Content-Type", "text/html");
 
-    res.write('<h1>hello yo</h1>')
-    res.end()
-})
+  fs.readFile("./index.html", (err, data) => {
+    if (err) console.log(err);
+    else {
+      res.write(data);
+      res.end();
+    }
+  });
+});
 
-server.listen(3000,'localhost',()=>{
-    console.log("listening for request on port number 3000")
-})
+server.listen(3000, "localhost", () => {
+  console.log("listening for request on port number 3000");
+});
