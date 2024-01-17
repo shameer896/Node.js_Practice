952a63f9dba16b7261d5008da7afe79735fbd460
Shameer Abdul Kader <shameerak896@gmail.com>
2024-01-17 19:51:13 +0530

    using ejs files to display content

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
-const express = require('express')
+const express = require("express");
 //express app
-const app = express()
+const app = express();
+
+//register view engine
+app.set("view engine", "ejs");
 
 //listen for request
-app.listen(3000)
+app.listen(3000);
 
-app.get('/',(req,res)=>{
-    res.send('<h1>hi</h1>')
-})
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
-app.get('/about',(req,res)=>{
-    res.send('<h1>About</h1>')
-})
+app.get("/about", (req, res) => {
+  res.render("index");
+});
 
-app.use((req,res)=>{
-    res.sendFile('./404.html',{ root : __dirname})
-})
+app.use((req, res) => {
+  res.render("404");
+});
