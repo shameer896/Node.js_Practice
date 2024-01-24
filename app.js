b7c5e2a188e8b4c59b108543ca96a59485303cbf
Shameer Abdul Kader <shameerak896@gmail.com>
2024-01-24 15:52:45 +0530

    Saving data in mongodb

--- app.js
+++ app.js
@@ -2,14 +2,16 @@ const express = require("express");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
 const { result } = require("lodash");
+const Blog = require("./models/blog");
 
 //connect to mongo db
 const db =
-  "mongodb+srv://user1:test1234@cluster0.qn9jiea.mongodb.net/?retryWrites=true&w=majority";
+  "mongodb+srv://user1:test1234@cluster0.qn9jiea.mongodb.net/blog-documents?retryWrites=true&w=majority";
 mongoose
   .connect(db)
   .then((result) => console.log("db connected"))
   .catch((err) => console.log(err));
+
 //express app
 const app = express();
 
@@ -28,6 +30,23 @@ app.get("/", (req, res) => {
 
 app.get("/about", (req, res) => {
   res.render("index");
+});
+
+app.get("/add-blog", (req, res) => {
+  const blog = new Blog({
+    title: "new",
+    snippet: "about",
+    body: "more about",
+  });
+
+  blog
+    .save()
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
 app.use((req, res) => {
